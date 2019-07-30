5c70514a66d67c6ec2fd56831635570e51a82e75
Austin Gao <ag758@cornell.edu>
2019-07-29 20:38:38 -0400

    .

--- TicketHawk/CustomerMainViewController.swift
+++ TicketHawk/CustomerMainViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CustomerMainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,17 +21,11 @@ class CustomerMainViewController: UIViewController {
         self.navigationItem.titleView = imageView
         
         // Do any additional setup after loading the view.
+        
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
